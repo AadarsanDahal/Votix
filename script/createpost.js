4157cfad72966954e15ee01e8b9a979128a3571f
n1meshh <nimeshadhikari1216@gmail.com>
2025-07-20 22:36:18 +0545

    Add create post popup feature

--- script/createpost.js
+++ script/createpost.js
@@ -6,5 +6,18 @@ function openCreateModal() {
     document.getElementById("createPostModal").classList.add("hidden");
   }
   
-
-
+  document.getElementById('imageInput').addEventListener('change', function () {
+    const previewArea = document.getElementById('previewArea');
+    previewArea.innerHTML = ""; // Clear previous previews
+  
+    Array.from(this.files).forEach(file => {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const img = document.createElement('img');
+        img.src = e.target.result;
+        previewArea.appendChild(img);
+      };
+      reader.readAsDataURL(file);
+    });
+  });
+  
